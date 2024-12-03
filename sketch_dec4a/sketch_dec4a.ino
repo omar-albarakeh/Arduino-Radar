8fda8cc8c6d2676b21ef6e5a5b0e57fffe4ff20c
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-04 01:59:52 +0200

    calculateDistance function added

--- sketch_dec4a/sketch_dec4a.ino
+++ sketch_dec4a/sketch_dec4a.ino
@@ -4,6 +4,18 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int calculateDistance() { 
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  
+
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2; 
+  return distance;
+}
 
 }
